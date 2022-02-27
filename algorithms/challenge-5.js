9af73b5bf3aafefbce5e5b59fed19f2453c17830
sisterinthelord <100498307+sisterinthelord@users.noreply.github.com>
2022-02-27 11:35:28 +0100

    update challenge 5

--- algorithms/challenge-5.js
+++ algorithms/challenge-5.js
@@ -1,3 +1,3 @@
-function replaceDivisible(x, y) {}
+function replaceDivisible(x, y) {return num(x / y);}
 
-module.exports = replaceDivisible
+module.exports = replaceDivisible([2,4,3,6,5], 3)=>[2,4,"isDivisible", "isDivisible", 5];
